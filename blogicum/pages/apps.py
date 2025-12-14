f86cb7b0172961c68fa64abe2cd42991e1d941f2
Katerina Karaseva <k.karaseva2010@mail.ru>
2025-12-14 19:10:56 +0300

    up

--- blogicum/pages/apps.py
+++ blogicum/pages/apps.py
@@ -4,6 +4,6 @@ from django.apps import AppConfig
 
 class PagesConfig(AppConfig):
     """Конфигурация приложения 'pages'."""
-    
+
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'pages'
