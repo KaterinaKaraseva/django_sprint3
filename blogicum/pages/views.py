f25228fb82fb1023ff64cd5df1df04ee7d30fbab
Katerina Karaseva <k.karaseva2010@mail.ru>
2025-12-14 19:07:13 +0300

    change

--- blogicum/pages/views.py
+++ blogicum/pages/views.py
@@ -1,11 +1,14 @@
+"""Представления статических страниц."""
 from django.shortcuts import render
 
 
 def about(request):
+    """Страница "О проекте"."""
     template = 'pages/about.html'
     return render(request, template)
 
 
 def rules(request):
+    """Страница с правилами сайта."""
     template = 'pages/rules.html'
     return render(request, template)
